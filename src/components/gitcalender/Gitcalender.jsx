33a581ae2d41def9712deb5190737be353bcef7c
HARSHAL-AP <apsundeharshal129@gmail.com>
2022-12-12 21:27:29 +0530

    gitcalender

--- src/components/gitcalender/Gitcalender.jsx
+++ src/components/gitcalender/Gitcalender.jsx
@@ -7,8 +7,18 @@ export const GitHubCalendaar=() => {
 
 return (
 <div className='caleboady'>
+<div><GitHubCalendar username="HARSHAL-AP" /></div>
+<div><img  src="https://github-readme-stats.vercel.app/api/top-langs?username=harshal-ap&show_icons=true&locale=en&border_radius=2&theme=light" alt="harshal-ap" /></div>
+<div>
+<img  src="https://github-readme-stats.vercel.app/api?username=harshal-ap&show_icons=true&locale=en&border_radius=2&theme=light" alt="harshal-ap" /></div>
+<div>
+<img  src="https://github-readme-streak-stats.herokuapp.com/?user=harshal-ap&show_icons=true&locale=en&border_radius=2&theme=light" alt="harshal-ap" /></div>
+
+
+
     
-<GitHubCalendar username="HARSHAL-AP" />
+
+
 
 
 </div>
